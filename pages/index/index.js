3146cb663ac2f76c1527c568c0b14d931c778720
ZM <zhangpeng@imeduplus.com>
2020-12-15 20:12:39 +0800

    提交云数据库增删

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,27 +1,72 @@
  //新建项目时，都是基于官方的demo开始，教程从0开始、不基于官方demo
+ //云开发 分三步讲 初始化之后
+  /*  1、云数据库
+  * 2、云函数
+  * 3、云存储
+  */
  const DB = wx.cloud.database().collection("users");
  Page({
    data: {
      addResult: null,
-     openId: ""
+     openId: "",
+     list: [],
+     name: "",
+     age: null,
+     address: ""
+
    },
+   onShow() {
+     this.getUsers2();
+   },
+   handleName(e) {
+     this.setData({
+       name:e.detail.value
+     })
+   },
+   handleAge(e) {
+    this.setData({
+      age:Number(e.detail.value)
+    })
+  },
+  handleAddress(e) {
+    this.setData({
+      address:e.detail.value
+    })
+  },
    addData() {
-    DB.add({
+     const $this = this
+     DB.add({
        data: {
-         name: "海绵宝宝",
-         age: 103,
-         address: "海底"
+         name: this.data.name,
+         age: this.data.age,
+         address: this.data.address
        },
-       success(res){
-        console.log("addData",res)
+       success(res) {
+         $this.getUsers2();
+         $this.setData({
+          name:"",
+          age: null,
+          address: ""
+         })
+         console.log("addData", res.result)
        },
-       fail(err){
-        console.log("addData fail",err)
+       fail(err) {
+         console.log("addData fail", err)
        }
      })
    },
-   deleteData(){
-
+   deleteData(e) {
+     const $this = this
+     const id = e.target.dataset.id;
+     //doc只能查id?? 查下有没有where
+     DB.doc(id).remove({
+       success(res) {
+         debugger
+         console.log("删除成功");
+         $this.getUsers2();
+       },
+       fail(res) {}
+     })
    },
    qiuhe() {
      const $this = this
@@ -73,11 +118,14 @@
    getUsers2() {
      //修改权限为默认，依然可以获取成功
      //云函数可以实现推送、短信发送、邮件发送
-
+     const $this = this
      wx.cloud.callFunction({
        name: "getdata",
        success(res) {
          console.log("云函数获取", res)
+         $this.setData({
+           list: res.result.data
+         })
        },
        fail(err) {
          console.log(err)
